9dff752e869806c48e0f19d1afd1a4b0653aeffc
BensonLai <61774865+BensonLai@users.noreply.github.com>
2020-03-15 20:29:56 +0800

    Update hw1.py

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -3,7 +3,7 @@ import math
 x = int(input('請輸入本金: '))
 y = int(input('請輸入年數: '))
 r = float(input('請輸入年利率(%): '))
-c = math.ceil(x/y/12)
+c = math.ceil(x/y/12) %計算每期攤還本金在無條件進位
 listc = [c]*y*12
 listc[y*12-1] = x - c*(y*12-1)
 listx = [x]*y*12
@@ -21,5 +21,5 @@ for i in range(y*12):
 c1 = pd.DataFrame(listc, columns=["本金(元)"], index=ind)
 c2 = pd.DataFrame(listr, columns=["利息(元)"], index=ind)
 c3 = pd.DataFrame(listt, columns=["本金利息累計(元)"], index=ind)
-df = pd.concat([c1,c2,c3], axis=1)
-print(df)
+df = pd.concat([c1,c2,c3], axis=1) %將三行dataframe併在一起
+df
